49cb16cec71fa1cad33ea9c6d26b50b6adf8ada6
NastyaNed <acherkaeva1997@gmail.com>
2023-04-15 14:30:42 +0300

    git ignore

--- src/main/java/org/example/Calculation.java
+++ src/main/java/org/example/Calculation.java
@@ -1,2 +1,11 @@
-package org.example;public interface Calculation {
+package org.example;
+
+public interface Calculation {
+    double addition(double num1, double num2);
+
+    double substrction(double num1, double num2);
+
+    double multiplication(double num1, double num2);
+
+    double division(double num1, double num2);
 }
--- src/main/java/org/example/Sourse.java
+++ src/main/java/org/example/Sourse.java
@@ -1,2 +1,22 @@
-package org.example;public class Sourse {
+package org.example;
+
+public class Sourse {
+    private double num;
+    private String operator;
+
+    public double getNum() {
+        return num;
+    }
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
 }
